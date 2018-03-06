37d09b39d1f172a9a58debbc6c0600ac2739b610
evanbottomley <EvanBottomley@cmail.carleton.ca>
2018-03-05 21:19:01 -0500

    Adding unit tests for model objects
    I was unable to test these thoroughly, but this branch is in high demand and needs the tests before it can be merged.

---
+++ src/test/java/learningOutcomes/LearningOutcomeTest.java
@@ -0,0 +1,27 @@
+package learningOutcomes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LearningOutcomeTest {
+
+    LearningOutcome lo;
+	Category cat;
+
+    @Before
+    public void setUp() throws Exception {
+        lo = new LearningOutcome();
+        cat = new Category("TestCategory");
+    }
+
+	@Test
+    public void setValuesTest() throws Exception {
+		lo.setId(1);
+		lo.setName("TestName");
+		lo.setCategory(cat);
+		Assert.assertEquals(1, lo.getId());
+		Assert.assertEquals("TestName", lo.getName());
+		Assert.assertEquals("TestCategory", lo.getCategory().getName());
+	}
+}
---
+++ src/test/java/learningOutcomes/ProgramTest.java
@@ -0,0 +1,42 @@
+package learningOutcomes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProgramTest {
+
+    Course course;
+    Program program;
+
+    @Before
+    public void setUp() throws Exception {
+        course = new Course();
+        program = new Program();
+    }
+
+	@Test
+    public void setValuesTest() throws Exception {
+		program.setId(1);
+		program.setName("TestName");
+		Assert.assertEquals(1, program.getId());
+		Assert.assertEquals("TestName", program.getName());
+	}
+	
+    @Test
+    public void addCourseTest() throws Exception {
+        Assert.assertEquals(0, program.getSize());
+        program.addCourse(course);
+        Assert.assertEquals(1, program.getSize());
+        Assert.assertEquals("Default", program.getCourse(0).getName());
+    }
+
+    @Test
+    public void removeCourseTest() throws Exception {
+        program.addCourse(course);
+        Assert.assertEquals(1, program.getSize());
+        program.removeCourseIndex(0);
+        Assert.assertEquals(0, program.getSize());
+    }
+
+}
