de10907ccc402a0f427d14460c08e0195c286a07
Monty1029 <dhanani.monty@gmail.com>
2018-03-20 12:38:50 -0400

    Added form to create categories

---
+++ src/main/resources/static/categories.js
@@ -0,0 +1,29 @@
+var EMPTY_HTML = '';
+var CATEGORIES_BASE_PATH = '/categories';
+
+var handleCreateCategoryFormSubmission = (e) => {
+    e.preventDefault();
+
+    var categoryData = {};
+    var inputs = $('form#categoryForm :input').serializeArray().forEach((input) => {
+        categoryData[input.name] = input.value;
+    });
+
+    $.ajax({
+        url: CATEGORIES_BASE_PATH,
+        type:'POST',
+        data: JSON.stringify(categoryData),
+        contentType:'application/json',
+        dataType:"json",
+        success: (result) => {console.log(result)}
+    });
+};
+
+
+var setUp = () => {
+
+    $('#categoryForm').submit(handleCreateCategoryFormSubmission);
+};
+
+
+$(setUp);
