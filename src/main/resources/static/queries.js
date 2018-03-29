f1994a7fc9115a49b769af25ca9c513ece27e25d
Garrett Steele <gsteelex@users.noreply.github.com>
2018-03-29 16:02:34 -0400

    Milestone 3  - Implement "queryCoursesInProgramYear"  - Implement building of table from passed in headers and 2D data array

--- src/main/resources/static/queries.js
+++ src/main/resources/static/queries.js
@@ -8,6 +8,7 @@ var PROGRAMS_BASE_PATH = '/programs';
 var COURSES_BASE_PATH = '/courses';
 var CATEGORY_BASE_PATH = '/categories';
 
+
 var populateProgramToQueryList = () => {
     $(PROGRAM_SELECT_ID).html(EMPTY_HTML);
 
@@ -30,7 +31,35 @@ var populateCourseToQueryList = () => {
 
 var queryCoursesInProgramYear = () => {
     console.log('to be implemented 1');
-    //TODO
+    var programYear = +$(PROGRAM_YEAR_ID).val();    //auto converts to a number when adding the "+" sign
+    var programId =+$(PROGRAM_SELECT_ID).val();       //auto converts to a number when adding the "+" sign
+    var tableHeadings = ['Program', 'Program ID', 'Year', 'Course', 'Learning Outcomes'];
+
+
+    $.get(PROGRAMS_BASE_PATH, (programs) => {
+        var programsInYear = [];
+
+        programs.forEach((program) => {
+            program.courses.forEach((course) => {
+
+                //if the course is from the corresponding project year it needs to be displayed
+                if (course.year === programYear) {
+                    //get data of course
+                    var data = [];
+                    data.push(program.name);
+                    data.push(program.id);
+                    data.push(course.year);
+                    data.push(course.name);
+                    data.push(course.learningOutcomes);
+
+                    //add to list of courses
+                    programsInYear.push(data);
+                }
+            });
+        });
+
+        displayTable(tableHeadings, programsInYear);
+    });
 };
 
 var queryLearningOutcomesOfProgramYear = () => {
@@ -46,9 +75,28 @@ var queryCoursesOfCategory = () => {
 
 //data is a 2d array, rows and items desired in each table column corresponding to the row
 var displayTable = (headerArray, data) => {
-    console.log(headerArray);
-    console.log(data);
-    //TODO
+    console.log(headerArray);   //TODO: remove before PR
+    console.log(data);          //TODO: remove before PR
+
+    $(QUERY_RESULT_TABLE).html(EMPTY_HTML);
+
+    //create and append header
+    var tableHeader = $('<tr></tr>');
+    headerArray.forEach((header) => {
+        tableHeader.append($('<th>' + header + '</th>'));
+    });
+    $(QUERY_RESULT_TABLE).append(tableHeader);
+
+    //create table rows and append them
+    data.forEach((row) => {
+        var tr = $('<tr></tr>');
+
+        row.forEach((col) => {
+            tr.append($('<td>' + col + '</td>'))
+        });
+
+        $(QUERY_RESULT_TABLE).find('tbody').append(tr);
+    });
 };
 
 var setUp = () => {
